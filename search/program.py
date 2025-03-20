5eb24bcef1f0804b6b2d7904c756aa6b953d5de8
Zuhair Mobasshar <146693112+zuhairmob@users.noreply.github.com>
2025-03-20 17:04:19 +0500

    Add files via upload
    initial commit

---
+++ search/program.py
@@ -0,0 +1,48 @@
+# COMP30024 Artificial Intelligence, Semester 1 2025
+# Project Part A: Single Player Freckers
+
+from .core import CellState, Coord, Direction, MoveAction
+from .utils import render_board
+
+
+def search(
+    board: dict[Coord, CellState]
+) -> list[MoveAction] | None:
+    """
+    This is the entry point for your submission. You should modify this
+    function to solve the search problem discussed in the Part A specification.
+    See `core.py` for information on the types being used here.
+
+    Parameters:
+        `board`: a dictionary representing the initial board state, mapping
+            coordinates to "player colours". The keys are `Coord` instances,
+            and the values are `CellState` instances which can be one of
+            `CellState.RED`, `CellState.BLUE`, or `CellState.LILY_PAD`.
+    
+    Returns:
+        A list of "move actions" as MoveAction instances, or `None` if no
+        solution is possible.
+    """
+
+    # The render_board() function is handy for debugging. It will print out a
+    # board state in a human-readable format. If your terminal supports ANSI
+    # codes, set the `ansi` flag to True to print a colour-coded version!
+    print(render_board(board, ansi=True))
+
+    # Do some impressive AI stuff here to find the solution...
+    # ...
+    # ... (your solution goes here!)
+    # ...
+
+    # Here we're returning "hardcoded" actions as an example of the expected
+    # output format. Of course, you should instead return the result of your
+    # search algorithm. Remember: if no solution is possible for a given input,
+    # return `None` instead of a list.
+    return [
+        MoveAction(Coord(0, 5), [Direction.Down]),
+        MoveAction(Coord(1, 5), [Direction.DownLeft]),
+        MoveAction(Coord(3, 3), [Direction.Left]),
+        MoveAction(Coord(3, 2), [Direction.Down, Direction.Right]),
+        MoveAction(Coord(5, 4), [Direction.Down]),
+        MoveAction(Coord(6, 4), [Direction.Down]),
+    ]
